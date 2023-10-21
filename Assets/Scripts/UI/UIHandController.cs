c17f076757236f22b9a4ccf500b0bd556d651a6c
Carlos <carlos.enriquez.valerio@gmail.com>
2023-10-21 06:12:38 -0600

    select card

--- Assets/Scripts/UI/UIHandController.cs
+++ Assets/Scripts/UI/UIHandController.cs
@@ -6,8 +6,11 @@ using UnityEngine.UI;
 public class UIHandController : MonoBehaviour
 {
     [SerializeField] private GameObject _cardHandPrefab;
+    [SerializeField] private GameObject _cardOnHandPrefab;
+    [SerializeField] private GameObject _uiHandHolder;
     private DeckController _deck;
     private GameObject[] _currentHand= new GameObject[5];
+    private GameObject _currentCardSelected;
     private void Awake()
     {
         _deck = FindObjectOfType<DeckController>();
@@ -19,6 +22,11 @@ public class UIHandController : MonoBehaviour
     void Update()
     {
 
+    }
+    public void OnPress()
+    {
+        _uiHandHolder.SetActive(false);
+        
     }
     public void DisplayCard()
     {
